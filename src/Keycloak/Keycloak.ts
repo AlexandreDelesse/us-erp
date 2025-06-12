d822a7b8f8f06d2a0c87bd56f6504b9fd9affa3a
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2025-06-12 11:00:39 +0200

    update keycloak id

--- src/Keycloak/Keycloak.ts
+++ src/Keycloak/Keycloak.ts
@@ -4,7 +4,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
   url: import.meta.env.KEYClOAK_HOST || "https://auth.ade-dev.fr/", // ou juste '/realms' si Keycloak 18+
   realm: import.meta.env.KEYCLOAK_REALM || "ustest",
-  clientId: import.meta.env.KEYCLOAK_CLIENT_ID || "us-mecanic",
+  clientId: import.meta.env.KEYCLOAK_CLIENT_ID || "us-erp",
 });
 
 export default keycloak;
