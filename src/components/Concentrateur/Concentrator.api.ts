7db27f2e670e75d18aeced8c8fce2fcf323966cd
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2025-06-12 14:11:35 +0200

    app concentrator api

--- src/components/Concentrateur/Concentrator.api.ts
+++ src/components/Concentrateur/Concentrator.api.ts
@@ -1,35 +1,36 @@
+import client from "../../api/client";
 import type { Concentrateur } from "./Concentrateur.model";
 
 export const getConcentrator = async () => {
-  //   return (await client.get("Mutuelle")).data;
-  return await JSON.parse(localStorage.getItem("concentrators") ?? "[]");
+  return (await client.get("Concentrator")).data;
+  // return await JSON.parse(localStorage.getItem("concentrators") ?? "[]");
 };
 
 export const deleteConcentrator = async (id: number) => {
-  //   return (await client.delete("Mutuelle/" + id)).data;
-  const mutuelles = await getConcentrator();
-  return localStorage.setItem(
-    "concentrators",
-    JSON.stringify(mutuelles.filter((c: Concentrateur) => c.Id != id))
-  );
+  return (await client.delete("Concentrator/" + id)).data;
+  // const mutuelles = await getConcentrator();
+  // return localStorage.setItem(
+  //   "concentrators",
+  //   JSON.stringify(mutuelles.filter((c: Concentrateur) => c.Id != id))
+  // );
 };
 
 export const postConcentrator = async (cmd: Concentrateur) => {
-  //   return (await client.post("Mutuelle", cmd)).data;
-  const mutuelles = await getConcentrator();
-  return localStorage.setItem(
-    "concentrators",
-    JSON.stringify([...mutuelles, { ...cmd, id: mutuelles.length + 1 }])
-  );
+  return (await client.post("Concentrator", cmd)).data;
+  // const mutuelles = await getConcentrator();
+  // return localStorage.setItem(
+  //   "concentrators",
+  //   JSON.stringify([...mutuelles, { ...cmd, id: mutuelles.length + 1 }])
+  // );
 };
 
 export const putConcentrator = async (mutuelle: Concentrateur) => {
-  //   return (await client.put("Mutuelle", mutuelle)).data;
-  const mutuelles = await getConcentrator();
-  return localStorage.setItem(
-    "mutuelles",
-    JSON.stringify(
-      mutuelles.map((c: Concentrateur) => (c.Id == mutuelle.Id ? mutuelle : c))
-    )
-  );
+  return (await client.put("Concentrator", mutuelle)).data;
+  // const mutuelles = await getConcentrator();
+  // return localStorage.setItem(
+  //   "mutuelles",
+  //   JSON.stringify(
+  //     mutuelles.map((c: Concentrateur) => (c.Id == mutuelle.Id ? mutuelle : c))
+  //   )
+  // );
 };
